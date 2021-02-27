eaa6d4e7fcbbfccf33013a30705ab31649037c02
AntNatx <anthony.hernandez@g.austincc.edu>
2021-02-26 20:44:29 -0600

    HW 3 commit

--- src/homework/03_decisions/decisions.cpp
+++ src/homework/03_decisions/decisions.cpp
@@ -46,3 +46,59 @@ double calculate_gpa(int credit_hours, double credit_points)
 	return credit_points / credit_hours;
 }
 
+std::string get_letter_grade_using_if(int grade)
+{
+	if (grade >= 90 && grade <= 100) 
+	{
+		return "A";
+	}
+	else if(grade >= 80 && grade <= 89) 
+	{
+		return "B";
+	}
+	else if (grade >= 70 && grade <= 79)
+	{
+		return "C";
+	}
+	else if (grade >= 60 && grade <= 69 )
+	{
+		return "D";
+	}
+	else 
+	{
+		return "F";
+	}
+}
+
+
+std::string get_letter_grade_using_switch(int grade)
+{
+	std::string letter_grade;
+	
+	switch (grade)
+	{
+	case 90 ... 100:
+		letter_grade = "A";
+		break;
+
+	case 80 ... 89:
+		letter_grade = "B";
+		break;
+
+	case 70 ... 79:
+		letter_grade = "C";
+		break;
+
+	case 60 ... 69:
+		letter_grade = "D";
+		break;
+
+	case 0 ... 59:
+		letter_grade = "F";
+		break;
+
+	default:
+		letter_grade = "Not a valid grade";	
+	}
+	return letter_grade;
+}
