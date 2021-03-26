4c8b66bc7c41669656ea17cdcbbc07ea5c35bb5e
AntNatx <anthony.hernandez@g.austincc.edu>
2021-03-26 08:45:37 -0500

    Homework 5

--- src/homework/04_iteration/dna.cpp
+++ src/homework/04_iteration/dna.cpp
@@ -1,4 +1,24 @@
 #include "dna.h"
+#include <string>
+
+using std::string;
+
+
+
+double get_gc_content(const string& dna)
+{
+    
+    
+    double gAndc = 0;
+
+    for (std::size_t count = 0; count < dna.size(); count++)
+    {   
+        if (dna [count] == 'G' || dna [count] == 'C')
+        gAndc = gAndc + 1;
+
+    }   
+    return gAndc / dna.size();
+}
 /*
 Write code for function get_gc_content that accepts
 a const reference string parameter and returns a double.
@@ -8,14 +28,49 @@ Return quotient.
 */
 
 
+std::string get_reverse_string(string dna)
+{
+    string reverseDna;
+    
+    for (int count = (dna.size() - 1); count >= 0; count -- )
+    {
+        reverseDna.push_back( dna[count] );
+    }
 
+    return reverseDna;
+}
 
 /*
 Write code for function get_reverse_string that
 accepts a string parameter and returns a string reversed.
 */
+std::string get_dna_complement(string dna)
+{
+    string reverseDNa = get_reverse_string(dna);
 
+    for (std::size_t count = 0; count < dna.size(); count ++ )
+    {
+        if (reverseDNa[count] == 'A')
+        {
+            reverseDNa[count] == 'T';
+        }
+    
+        else if (reverseDNa[count] == 'T')
+        {
+            reverseDNa[count] == 'A';
+        }
 
+        else if (reverseDNa[count] == 'C')
+        {
+            reverseDNa[count] == 'G';
+        }
+
+        else {reverseDNa[count] == 'C';
+        }
+    }
+    
+    return reverseDNa;
+}
 
 /*
 Write prototype for function get_dna_complement that
--- src/homework/04_iteration/main.cpp
+++ src/homework/04_iteration/main.cpp
@@ -1,7 +1,11 @@
 //write include statements
-
+#include "dna.h"
+#include <string>
+#include <iostream>
 //write using statements
-
+using std::string;
+using std::cin;
+using std::cout;
 
 /*
 Write code that prompts user to enter 1 for Get GC Content, 
@@ -12,5 +16,40 @@ user enters a y or Y.
 */
 int main() 
 {
+	std::string dna;
+	int decision;
+	char keepGoing = 'y';
+
+	while (keepGoing == 'y' || keepGoing == 'Y')
+
+	{
+	
+		cout<<"Please enter 1 for get GC content, or 2 for Get DNA Complement: \n";
+		cin>>decision;
+
+		if (decision == 1)
+		{
+			cout<<"Please enter a DNA string: \n";
+			cin>>dna;
+			cout<<"the GC content is: "<< get_gc_content(dna)<<"\n";
+
+			cout<<"Would you like to run a different test? Enter Y or any other key to exit: \n";
+			cin>>keepGoing;
+		}
+		else if (decision == 2)
+		{
+			cout<<"Please enter a DNA string: \n";
+			cin>>dna;
+			cout<<"The DNA complement is: "<<get_dna_complement(dna)<<"\n";
+			cout<<"Would you like to run a different test? Enter Y or any other key to exit: \n";
+			cin>>keepGoing;
+		}
+		else{
+			cout<<"You have entered an invalid selection.\n";
+			cout<<"Would you like to run a different test? Enter Y or any other key to exit: \n";
+			cin>>keepGoing;
+		}
+	}
+	
 	return 0;
 }
