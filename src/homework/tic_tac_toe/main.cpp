1aa7f8e9d198d7d0f99bde165b30e9a7bda75db0
AntNatx <anthony.hernandez@g.austincc.edu>
2021-04-17 08:33:06 -0500

    HW 8

--- src/homework/tic_tac_toe/main.cpp
+++ src/homework/tic_tac_toe/main.cpp
@@ -1,4 +1,5 @@
 #include "tic_tac_toe.h"
+#include "tic_tac_toe_manager.h"
 #include <iostream>
 using std::cout;
 using std::cin;
@@ -7,53 +8,20 @@ int main()
 {
 	// create an instance of tictactoe
 	TicTacToe game;
-	
-	//declare variable
-	string first_player;
-	
-	//outer loop of game
-	char prompt = 'y';
-	
-	//prompt the user for first player
-	cout<<"Player 1 enter a capital X or an O to select your choice: \n";
-	cin>>first_player;
-	if (first_player != "X" && first_player != "O")
-	{
-		cout<<"\n You did not enter a valid choice. Please enter an X or O: ";
-		cin>>first_player;	
-	}
-	//start the game
-	game.start_game (first_player);
-	//marking the spots
-	int position;
+	TicTacToeManager board;
 
-	while(prompt == 'y' || prompt == 'Y')
+	do
 	{
-	cout<<"\nPlease enter a position to mark (1-9): \n";
-	cin>>position;
-		
-	//input validation
-	if (position < 1 || position > 9)
-	{
-		cout<<"\n You did not enter a valid number, Please enter a value between 1-9: ";
-		cin>>position;
-	}
-		
-	game.mark_board(position);
-	//display the board
-		
-	game.display_board();
-	if (game.game_over() == true)
-	{
-		cout<<"Game Over\n";
-		cout<<game.get_winner()<< " is the winner";
-		cout<<"\nGame starting over. \n";
-		game.start_game(first_player);
-	}
-	//condition controlled loop
-	cout<<"Is next Player ready? Press Y or any other key to quit: \n";
-	cin>>prompt;
-	}
+		cin>>game;
+	
+	
+    	cout<<game;
+    
+	
+	} while (game.game_over() == false);
+	
+	cout<<board;
+	
 	
 	return 0;
 	
