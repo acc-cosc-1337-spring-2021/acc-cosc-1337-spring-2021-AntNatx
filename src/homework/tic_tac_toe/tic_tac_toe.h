9a29716e91d805a4a769446725482c2b12c7847b
AntNatx <anthony.hernandez@g.austincc.edu>
2021-04-30 22:14:26 -0500

    Hw 10

--- src/homework/tic_tac_toe/tic_tac_toe.h
+++ src/homework/tic_tac_toe/tic_tac_toe.h
@@ -8,19 +8,10 @@ using std::string;
 //h
 class TicTacToe
 {
-friend std::ostream& operator<<(std::ostream& out, TicTacToe& game);
-friend std::istream& operator>>(std::istream& in, TicTacToe& game);
-
-
-
 public:
 
     TicTacToe(int size):pegs(size*size, " "){};
-    TicTacToe(std::vector<string> p, string win)
-    {
-        pegs = p;
-        winner = win;
-    };//this will initialize some_vector to s*s elemeents of " "
+    TicTacToe(std::vector<string> p, string win) : pegs(p), winner(win){}//this will initialize some_vector to s*s elemeents of " "
 
     std::vector<string> get_pegs()const;
 
@@ -37,24 +28,29 @@ public:
 
     string get_winner() const;
 
+    std::vector<string>get_pegs()const{return pegs;}
+
+    friend std::ostream& operator<<(std::ostream& out, TicTacToe& game);
+    friend std::istream& operator>>(std::istream& in, TicTacToe& game);
+
 protected:
 
     std::vector<std::string> pegs;
 
-    virtual bool check_column_win()const;
+    virtual bool check_column_win();
 
-    virtual bool check_row_win()const;
+    virtual bool check_row_win();
 
-    virtual bool check_diagnol_win()const;
+    virtual bool check_diagnol_win();
 
 private:
+    string winner;
+    string player;
     
     void set_winner();
-    string winner;
 
     void set_next_player();
-    string player;
-
+    
     bool check_board_full();
 
     void clear_board();
